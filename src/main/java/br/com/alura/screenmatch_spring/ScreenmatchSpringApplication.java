d28d65e892f6bfffa27b445ee36658a75b7ef1d3
Rivas <rivaldo13@gmail.com>
2023-08-24 11:09:30 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch_spring/ScreenmatchSpringApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch_spring;
+
+import br.com.alura.screenmatch_spring.model.DadosSerie;
+import br.com.alura.screenmatch_spring.service.ConsumoApi;
+import br.com.alura.screenmatch_spring.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchSpringApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=lost&apikey=915aa981");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
